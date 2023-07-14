f819a566f2c2498717ac57462145f7124fa90c3f
rhogger <rhoggerrv@gmail.com>
2023-07-13 23:49:44 -0300

    :construction: ci: Lógica de temporizador em desenvolvimento /2

--- modules/buttons/stop-watch.js
+++ modules/buttons/stop-watch.js
@@ -1,14 +1,23 @@
 import { toggleBetweenPlayOrPause } from './play-pause.js'
-import { Time, minutesDisplay, secondsDisplay } from '../timer/time.js'
+import { Time, minutesDisplay, secondsDisplay, countdown } from '../timer/time.js'
 
 const btnStop = document.querySelector('#btn-stop')
 const btnWatch = document.querySelector('#btn-watch')
 
-btnStop.addEventListener('click', toggleBetweenPlayOrPause)
+btnStop.addEventListener('click', () => {
+  toggleBetweenPlayOrPause()
+  Time.finish = true
+})
 
 btnWatch.addEventListener('click', () => {
   Time.minutes = prompt('Quantos minutos deseja contar?')
+
   minutesDisplay.textContent = Number(Time.minutes)
+
+  if (Time.minutes < 10) {
+    minutesDisplay.textContent = '0' + Number(Time.minutes)
+  }
+
   secondsDisplay.textContent = '00'
 })
 
--- modules/timer/time.js
+++ modules/timer/time.js
@@ -1,6 +1,10 @@
+import { toggleBetweenStopOrWatch } from '../buttons/stop-watch.js'
+import { toggleBetweenPlayOrPause } from '../buttons/play-pause.js'
+
 export const Time = {
   minutes: '00',
   seconds: '05',
+  finish: false
 }
 
 export const minutesDisplay = document.querySelector('#minutes')
@@ -10,37 +14,52 @@ minutesDisplay.textContent = String(Time.minutes)
 secondsDisplay.textContent = String(Time.seconds)
 
 export function countdown() {
-  setTimeout(function () {
-    Time.minutes = Number(minutesDisplay.textContent)
-    Time.seconds = Number(secondsDisplay.textContent)
+  if (Time.finish === false) {
+    setTimeout(function () {
+      Time.minutes = Number(minutesDisplay.textContent)
+      Time.seconds = Number(secondsDisplay.textContent)
 
-    if (Time.seconds <= 0) {
-      Time.seconds = 60
-      Time.minutes--
-    }
+      if (Time.seconds <= 0) {
+        Time.seconds = 60
+        Time.minutes--
+      }
 
-    Time.seconds--
+      Time.seconds--
 
-    // console.log(minutesDisplay.textContent);
-    console.log(secondsDisplay.textContent);
+      // console.log(minutesDisplay.textContent);
+      console.log(secondsDisplay.textContent);
 
-    if (Time.minutes < 10) {
-      Time.minutes = '0' + Number(Time.minutes)
-    }
+      if (Time.minutes < 10) {
+        Time.minutes = '0' + Number(Time.minutes)
+      }
 
-    if (Time.seconds < 10) {
-      Time.seconds = '0' + Number(Time.seconds)
-      console.log(Time.seconds);
-    }
+      if (Time.seconds < 10) {
+        Time.seconds = '0' + Number(Time.seconds)
+        console.log(Time.seconds);
+      }
 
-    minutesDisplay.textContent = String(Time.minutes)
-    secondsDisplay.textContent = String(Time.seconds)
+      minutesDisplay.textContent = String(Time.minutes)
+      secondsDisplay.textContent = String(Time.seconds)
 
-    if (Time.minutes == 0 && Time.seconds == 0) {
-      console.log('Terminou');
-      return
-    }
+      if (Time.minutes == 0 && Time.seconds == 0) {
+        resetDisplayCountdown()
+        toggleBetweenPlayOrPause()
+        return
+      }
 
-    countdown()
-  }, 1000)
+      countdown()
+    }, 1000)
+
+  } else {
+    resetDisplayCountdown()
+    return
+  }
+}
+
+export function resetDisplayCountdown() {
+  Time.minutes = '00'
+  Time.seconds = '05'
+
+  minutesDisplay.textContent = String(Time.minutes)
+  secondsDisplay.textContent = String(Time.seconds)
 }
